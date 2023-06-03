56275ae79fb863b819aa46fc6da08815a79dcc55
sergey <69920319+sergeyrmain@users.noreply.github.com>
2023-06-03 15:28:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const User = require('./models/user');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
